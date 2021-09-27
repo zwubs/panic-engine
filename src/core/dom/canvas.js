179b6fcdec2b8bd2a88a2953c75799420613e39c
zwubs <zwinters00@gmail.com>
2021-09-27 08:27:19 -0400

    Create "core/dom"

---
+++ src/core/dom/canvas.js
@@ -0,0 +1,13 @@
+/**
+ *	@author zwubs
+ */
+
+import { Element } from './element.js';
+
+let Canvas = document.createElement('canvas');
+
+Canvas.id = "PANIC-Canvas";
+
+Element.appendChild( Canvas );
+
+export { Canvas };
---
+++ src/core/dom/element.js
@@ -0,0 +1,11 @@
+/**
+ *	@author zwubs
+ */
+
+let Element = document.createElement("div");
+
+Element.id = "PANIC";
+
+document.body.appendChild( Element );
+
+export { Element };
