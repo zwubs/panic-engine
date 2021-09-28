517b24f6c588df055292e35e1885de3b07f3ed56
zwubs <zwinters00@gmail.com>
2021-09-28 12:06:48 -0400

    Add lighting

--- src/core/rendering/scene.js
+++ src/core/rendering/scene.js
@@ -2,7 +2,7 @@
  *	@author zwubs
  */
 
-import { Scene as THREE_Scene, Color, FogExp2, HemisphereLight } from '../../lib/three.mjs';
+import { Scene as THREE_Scene, Color, FogExp2, HemisphereLight, DirectionalLight } from '../../lib/three132.js';
 
 class Scene extends THREE_Scene {
 
@@ -12,6 +12,24 @@ class Scene extends THREE_Scene {
 
 		this.background = new Color( 0x222222 );
 		this.fog = new FogExp2( new Color( 0x222222 ), 0.025 );
+
+		this.add( new HemisphereLight( 0xC9F8FF, 0xC9F8FF, 1.25 ) );
+
+		let dirLight = new DirectionalLight( 0xFFFFFF, 1.25 );
+		dirLight.castShadow = true;
+
+		dirLight.shadow.mapSize.width = 2048;
+		dirLight.shadow.mapSize.height = 2048;
+
+		dirLight.shadow.camera.near = 0.01;
+		dirLight.shadow.camera.far = 25;
+
+		dirLight.shadow.bias = -0.001;
+
+		dirLight.position.set( 0.5, 1, -0.75 );
+		dirLight.position.multiplyScalar( 3 );
+
+		this.add( dirLight );
 
 	}
 
