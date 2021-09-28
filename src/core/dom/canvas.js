bf989c6e4a7c0b1b7f41a33b7f4eeeda68c2a0a8
zwubs <zwinters00@gmail.com>
2021-09-28 12:07:30 -0400

    Add resize event

--- src/core/dom/canvas.js
+++ src/core/dom/canvas.js
@@ -3,10 +3,22 @@
  */
 
 import { Element } from './element.js';
+import { Camera } from '../rendering/camera.js';
+import { Renderer } from '../rendering/renderer.js';
 
 let Canvas = document.createElement('canvas');
 
 Canvas.id = "PANIC-Canvas";
+
+window.addEventListener("resize", function() {
+
+	Camera.aspect = window.innerWidth / window.innerHeight;
+    Camera.updateProjectionMatrix();
+
+	Renderer.setPixelRatio( window.devicePixelRatio );
+    Renderer.setSize( window.innerWidth, window.innerHeight );
+
+}, false);
 
 Element.appendChild( Canvas );
 
