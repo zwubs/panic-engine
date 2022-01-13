638b7adff4f30e8bd598f76cd5e66cd4b2f42f93
zwubs <zwinters00@gmail.com>
2022-01-13 14:14:58 -0500

    Add collision parser

---
+++ src/parsers/parser-collision.js
@@ -0,0 +1,40 @@
+/**
+ *	@author zwubs
+ */
+
+import { ColliderTemplate } from '../collision/collider-template.js'
+
+import { Box3, Vector3 } from 'three'
+
+export let CollisionParser = new function() {
+
+	this.parse = function( json ) {
+
+		let collider = new ColliderTemplate();
+
+		let size = new Vector3( 1, 1, 1 );
+		let position = new Vector3( 0, 0, 0 );
+
+		// Load Collision Boxes
+		for( const [ id, box ] of Object.entries( json.boxes ) ) {
+
+			if( box.size ) { size.set( box.size[0] / 16, box.size[1] / 16, box.size[2] / 16 ) }
+
+			if( box.offset ) { position.set( box.offset[0] / 16, box.offset[1] / 16, box.offset[2] / 16 ) }
+
+			collider.addBox( new Box3().setFromCenterAndSize( position, size ) );
+
+		}
+
+		// Create Bounding Box
+		// collider.calculateBoundingBox();
+
+		// Create Bounding Sphere
+		// collider.calculateBoundingSphere();
+
+		// Return to EntityTemplate
+		return collider;
+
+	}
+
+}
--- src/parsers/parsers.js
+++ src/parsers/parsers.js
@@ -12,4 +12,6 @@ export { CubeUVParser as CubeUV } from './parser-cube-uv.js';
 export { EntityModelParser as EntityModel } from './parser-entity-model.js';
 export { EntityArmatureParser as EntityArmature } from './parser-entity-armature.js';
 
+export { CollisionParser as Collision } from './parser-collision.js';
+
 export { ActionsParser as Actions } from './parser-actions.js';
