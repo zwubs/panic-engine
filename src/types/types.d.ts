119733def8ac55de72126c1c6b4535969d438a5b
zwubs <zwinters00@gmail.com>
2022-08-28 23:09:25 -0400

    Create types directory

---
+++ src/types/types.d.ts
@@ -0,0 +1,5 @@
+import { Tools } from '../panic'
+
+export type CardinalDirections = typeof Tools.CardinalDirections[number]
+
+export { JSON } from './json'
--- src/types/types.ts
+++
@@ -1,1 +0,0 @@
-export * as JSON from "./json.types"
