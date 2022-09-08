a0f21e1e954bc204a242de1d8e40cfab279aae11
zwubs <zwinters00@gmail.com>
2022-09-07 21:52:32 -0400

    Fix collision schema

--- src/schemas/collision.schema.ts
+++ src/schemas/collision.schema.ts
@@ -3,14 +3,10 @@ import { z } from 'zod'
 export const CollisionSchema = z.object({
     boxes: z.record(
         z.object({
-            cubes: z.array(
-                z.object({
-                    size: z.array(z.number()).length(3),
-                    offset: z.array(z.number()).length(3),
-                    rotation: z.array(z.number().min(0).max(360)).length(3),
-                    faces: z.string()
-                })
-            )
+            size: z.array(z.number()).length(3),
+            offset: z.array(z.number()).length(3),
+            rotation: z.array(z.number().min(0).max(360)).length(3),
         })
     )
 })
+
